1e10e264900765bb691e44d4f5565cf3e2e6e97a
gms63 <gms.fourpixel@gmail.com>
2022-06-22 14:55:19 -0300

    fix

--- server/prisma/migrations/20220622175502_cascate/migration.sql
+++ server/prisma/migrations/20220622175502_cascate/migration.sql
@@ -16,5 +16,5 @@ CREATE TABLE "lendings" (
     "fee" DECIMAL NOT NULL,
     "parcels" DECIMAL NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "lendings_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "clients" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "lendings_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "clients" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
